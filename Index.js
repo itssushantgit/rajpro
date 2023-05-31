b2620e77620a46a182ffb51c098e0c4f5678d1bb
itssushantgit <bharati.sushant9@gmail.com>
2023-05-31 23:01:24 +0545

    rajpro

---
+++ Index.js
@@ -0,0 +1,6 @@
+document.querySelector(`#login-btn`).onclick = () => {
+  document.querySelector(`.login_form`).classList.toggle(`active`);
+};
+// document.querySelector(`#close_form`).onclick = () => {
+//   document.querySelector(`.header`).classList.toggle(`active`);
+// };
